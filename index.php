2ed0dae696b3b573f69ef85d3e999cd5bfe15218
Mark Mendoza <mmendoza-rabara@mail.greenriver.edu>
2021-04-15 12:44:48 -0400

    created index file

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Mark Mendoza
+ * 4/15/21
+ * http://mmendoza-rabara.greenriverdev.com/328/pp2/index.php
+ * Pair Program 2
+ */
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+echo "<p>1)</p>";
+echo "<h3>Pair Program 2</h3>";
+
+
+
+
+?>
+
