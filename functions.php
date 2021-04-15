d9740b516e8350b561b56266e17d11f0956ba874
Mark Mendoza <mmendoza-rabara@mail.greenriver.edu>
2021-04-15 19:48:09 -0400

    removeDups attempt#1

--- functions.php
+++ functions.php
@@ -15,3 +15,11 @@ function largest($array) {
     }
     return $high;
 }
+
+function removeDups($array) {
+    $array = array_unique($array); //remove duplicates
+
+    foreach ($array as $item) {
+        echo "<p>$item</p>";
+    }
+}
